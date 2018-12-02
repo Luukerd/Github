b150d2a6046778e1bc27e16a35fc0c50ce980793
Stan <stvanbaarsen@gmail.com>
2018-12-02 20:03:33 +0100

    improve readability

--- Pages/JS/Main.js
+++ Pages/JS/Main.js
@@ -2,6 +2,6 @@
 
 $(document).ready(function(){
 	$.getJSON("https://api.github.com/users/stannl", function(data){
-		$("#main").html(data);
+		$("#main").html(JSON.stringify(data), null, "\t"););
 	});
 })
